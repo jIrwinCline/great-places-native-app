fedb90d5d8a0a7ae6ea285fde9143a09c379c7bb
jirwincline <jakobinwins@gmail.com>
2020-01-20 17:42:57 -0800

    had to manually install and tweak dependencies to work with expo**

--- App.js
+++ App.js
@@ -6,12 +6,3 @@ import PlacesNavigator from "./navigation/PlacesNavigator";
 export default function App() {
   return <PlacesNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
