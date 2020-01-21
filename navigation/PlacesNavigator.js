c0d310abbf7775b181a265952ed796f11ca27a74
jirwincline <jakobinwins@gmail.com>
2020-01-20 18:09:55 -0800

    add navigation header styles

--- navigation/PlacesNavigator.js
+++ navigation/PlacesNavigator.js
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+
 import PlacesListScreen from "../screens/PlacesListScreen";
 import PlaceDetailScreen from "../screens/PlaceDetailScreen";
 import NewPlaceScreen from "../screens/NewPlaceScreen";
@@ -17,7 +18,7 @@ const PlacesNavigator = createStackNavigator(
   },
   {
     defaultNavigationOptions: {
-      headersStyle: {
+      headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : ""
       },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../components/HeaderButton";
 
 const PlacesListScreen = () => {
   return (
@@ -7,6 +10,23 @@ const PlacesListScreen = () => {
       <Text>PlacesListScreen</Text>
     </View>
   );
+};
+
+PlacesListScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "All Places",
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add Place"
+          iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+          onPress={() => {
+            navData.navigation.navigate("NewPlace");
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({});
